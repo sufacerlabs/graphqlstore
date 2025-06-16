5f4a12fdcc981f1b10786cc61f45b893db009c1c
Andreeo <andreeodg7@outlook.es>
2025-06-16 04:29:11 +0200

    feat: crear e implementar el comando inicializar y realizar las pruebas

---
+++ source/cli/inicializar/__init__.py
@@ -0,0 +1,10 @@
+"""Modulo del inicializar"""
+
+from .comando_inicializar import ComandoInicializar
+from .main import inicializar
+
+
+__all__ = [
+    "ComandoInicializar",
+    "inicializar",
+]
--- source/cli/main.py
+++ source/cli/main.py
@@ -1,6 +1,6 @@
 "Modulo CLI para GraphQLStore"
 
-# from .inicializar.comando_inicializar import ComandoInicializar
+from .inicializar.comando_inicializar import ComandoInicializar
 from .conexion.comando_conexion import ComandoConexion
 from .probar_conexion.comando_probar_conexion import ComandoProbarConexion
 from .core import ConstructorCLI
@@ -18,7 +18,7 @@ class CLI:
         self.args = None
         self.comando_conexion = ComandoConexion()
         self.comando_probar_conexion = ComandoProbarConexion()
-        # self.comando_inicializar = ComandoInicializar()
+        self.comando_inicializar = ComandoInicializar()
 
     def parsear_comando(self):
         """
@@ -26,13 +26,13 @@ class CLI:
             la interfaz de línea de comandos."""
         self.constructor.agregar_comando(self.comando_conexion)
         self.constructor.agregar_comando(self.comando_probar_conexion)
-        # self.constructor.agregar_comando(self.comando_inicializar)
+        self.constructor.agregar_comando(self.comando_inicializar)
 
     def lanzamiento_condicionado(self):
         """Metodo que lanza el comando solicitado"""
         self.comando_conexion.contenido_comando(self.args)
         self.comando_probar_conexion.contenido_comando(self.args)
-        # self.comando_inicializar.contenido_comando(self.args)
+        self.comando_inicializar.contenido_comando(self.args)
 
     def ejecutar(self):
         """Metodo para ejecutar la interfaz de line de comandos"""
