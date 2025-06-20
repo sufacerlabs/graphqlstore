1a763344402506b6a731bd87a81e784c4b4a4333
Andreeo <andreeodg7@outlook.es>
2025-06-20 22:48:41 +0200

    build: actualizar version de empaquetado

--- source/__init__.py
+++ source/__init__.py
@@ -1,12 +1,4 @@
 """GraphQLStore - Un GraphQL CLI para gestionar bases de datos de forma fácil
 y eficiente."""
 
-from cli.main import CLI
-from main import main
-
 __author__ = "adg1023"
-
-__all__ = [
-    "CLI",
-    "main",
-]
